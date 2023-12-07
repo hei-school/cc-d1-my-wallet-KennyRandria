73ded5f1b5642ddf25bf20f4e4cd6198cf66b183
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2023-12-07 15:53:05 +0300

    feat : initialise project PHP

---
+++ Wallet.php
@@ -0,0 +1,45 @@
+
+class Portfolio {
+    private $balance;
+    private $username;
+    private $password;
+
+    public function __construct($username, $password) {
+        $this->balance = 0;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function deposit($amount) {
+        $this->authenticate();
+        $this->balance += $amount;
+        echo "Deposit successful\n";
+    }
+
+    public function withdraw($amount) {
+        $this->authenticate();
+        if ($amount <= $this->balance) {
+            $this->balance -= $amount;
+            echo "Withdrawal successful\n";
+        } else {
+            echo "Insufficient funds\n";
+        }
+    }
+
+    public function getBalance() {
+        $this->authenticate();
+        return $this->balance;
+    }
+
+    private function authenticate() {    
+        echo "Enter username: ";
+        $inputUsername = trim(fgets(STDIN));
+        echo "Enter password: ";
+        $inputPassword = trim(fgets(STDIN));
+
+        if ($this->username !== $inputUsername || $this->password !== $inputPassword) {
+            echo "Authentication failed. Access denied.\n";
+            exit(1); 
+        }
+    }
+}
